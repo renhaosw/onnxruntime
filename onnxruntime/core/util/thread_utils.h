c328eec67dd7b4e0f34b105196f618a5af66774e
Changming Sun <chasun@microsoft.com>
2020-03-18 16:09:24 -0700

    update

--- onnxruntime/core/util/thread_utils.h
+++ onnxruntime/core/util/thread_utils.h
@@ -1,10 +1,13 @@
 #include "core/platform/threadpool.h"
+#include "core/session/onnxruntime_c_api.h"
 #include <memory>
 #include <string>
 
 namespace onnxruntime {
 namespace concurrency {
 
-std::unique_ptr<ThreadPool> CreateThreadPool(const std::string& name, int thread_pool_size);
+
+std::unique_ptr<ThreadPool> CreateThreadPool(Env* env, ThreadPoolParams options,
+                                             Eigen::Allocator* allocator = nullptr);
 }  // namespace concurrency
 }  // namespace onnxruntime
